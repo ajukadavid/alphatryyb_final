5d0030267fdf7be2c7ffd7836838a7259c4984f0
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-16 23:30:48 +0200

    trynna get image

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { TroisJSVuePlugin } from 'troisjs';
+
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(TroisJSVuePlugin).use(router).mount('#app')
