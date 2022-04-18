0cb8d38f4cc9017a65e37d73c8f8b5a969c0a3bc
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-19 01:40:43 +0200

    added czukudi image and black border

--- src/views/ArtistView.vue
+++ src/views/ArtistView.vue
@@ -1,6 +1,15 @@
 <template>
 <div class="w-full h-screen border-8 border-black" >
-  Element
+  <div class="max-w-sm w-64 absolute left-12 top-16 bg-white border border-black shadow-md dark:bg-black dark:border-black">
+    <a href="#">
+      <img class="rounded-t-lg" src="../assets/Images/kenneth.jpg" alt="kenneth" />
+    </a>
+    <div class="bg-black p-2">
+      <a href="#">
+        <h5 class="text-lg font-bold tracking-tight text-gray-900 dark:text-white">CZUKUDI</h5>
+      </a>
+    </div>
+  </div>
 </div>
 </template>
 
