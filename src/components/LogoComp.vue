5081ef5a4b86545852fa74a558940494f5039a27
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-19 10:51:53 +0200

    added prompt

--- src/components/LogoComp.vue
+++ src/components/LogoComp.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="justify-center flex bg-black items-center h-screen">
-    <div class="text-4xl">
+    <div class="text-4xl grid">
       <router-link to="/artists">
-        <img src="../assets/Images/alphatryyb.jpg" class="w-20 animate-pulse">
+        <img alt="alphatryyb logo" src="../assets/Images/alphatryyb.jpg" class="w-20 mx-20 animate-bounce">
+        <h3 class="text-white prompt-text">Click to proceed</h3>
       </router-link>
+
     </div>
   </div>
 </template>
@@ -15,45 +17,8 @@ export default {
 </script>
 
 <style scoped>
-/* Dropdown Button */
-.dropbtn {
-  background-color: black;
-  color: white;
-  padding: 15px;
-  border: none;
-}
-
-/* The container <div> - needed to position the dropdown content */
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-/* Dropdown Content (Hidden by Default) */
-.dropdown-content {
-  display: none;
-  position: absolute;
-  right: 0px;
-  background-color: black;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-/* Links inside the dropdown */
-.dropdown-content a {
-  color: white;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-/* Change color of dropdown links on hover */
-.dropdown-content a:hover {background-color: #ddd; color: black}
-
-/* Show the dropdown menu on hover */
-.dropdown:hover .dropdown-content {display: block;}
-
-/* Change the background color of the dropdown button when the dropdown content is shown */
-.dropdown:hover .dropbtn {background-color: black;}
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap');
+ h3 {
+   font-family: 'Roboto Condensed', sans-serif;
+ }
 </style>
